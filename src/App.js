5abd1a17da9e83bb901d7f25161368c1370107d3
Hasan TOKER <75892548+tokerto7001@users.noreply.github.com>
2021-08-04 12:27:10 +0300

    Creation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Card from './components/Card';
 
 function App() {
+
+  // const options = {
+  //   method: 'GET',
+  //   url: 'https://streaming-availability.p.rapidapi.com/search/basic',
+  //   params: {
+  //     country: 'us',
+  //     service: 'netflix',
+  //     type: 'movie',
+  //     genre: '18',
+  //     page: '1',
+  //     language: 'en'
+  //   },
+  //   headers: {
+  //     'x-rapidapi-key': '73b9c671c3msh7e6990af8749c16p15dc5ajsn89b24cd29a7e',
+  //     'x-rapidapi-host': 'streaming-availability.p.rapidapi.com'
+  //   }
+  // };
+
+  // axios.request(options).then((response) => console.log(response))
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Card />
+      <Footer />
     </div>
   );
 }
