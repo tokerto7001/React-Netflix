7ef84d1d63cc8328f11a70cc0cdd3c31337e2102
Hasan TOKER <75892548+tokerto7001@users.noreply.github.com>
2021-08-10 13:00:47 +0300

    navbar first commits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>
-            Header
+        <div className='navbar'>
+            <div className='navbar-image-div'><img className='netflix' src='netflix.png' alt='netflix-logo' /></div>
+            <div className='navbar-navigations-div'>
+                <h1>About</h1>
+                <h1>Contact</h1>
+                <i className="fas fa-user-alt fa-2x"></i>
+            </div>
         </div>
     )
 }
